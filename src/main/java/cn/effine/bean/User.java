240314ac4dad9aaeee1d6feb6ad809e94a86c06a
effine <iballader@gmail.com>
2017-01-01 07:59:56 +0800

    add

---
+++ src/main/java/cn/effine/bean/User.java
@@ -0,0 +1,11 @@
+package cn.effine.bean;
+
+/**
+ * @author effine
+ * @Date 2017-01-01 07:58
+ * @email iballader#gmail.com
+ * @site http://www.effine.cn
+ * @sine 0.2
+ */
+public class User {
+}
