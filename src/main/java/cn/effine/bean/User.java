2d4b3e99ec5e32733275ae617e27bdacbfff210e
effine <iballader@gmail.com>
2017-01-02 08:02:27 +0800

    generate setter getter method

--- src/main/java/cn/effine/bean/User.java
+++ src/main/java/cn/effine/bean/User.java
@@ -8,5 +8,22 @@ package cn.effine.bean;
  * @sine 0.2
  */
 public class User {
-    
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
